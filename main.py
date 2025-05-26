dee012d0016351407ed1215684a7ccad9db94ba6
DDDDDDDDDDDDDDDDhinachansaikou <hank910451@gmail.com>
2025-05-26 15:50:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import time
 
-# 每 10 秒自動重新整理畫面
+# 每 10 秒自動刷新頁面
 if "last_refresh_time" not in st.session_state:
     st.session_state.last_refresh_time = time.time()
 elif time.time() - st.session_state.last_refresh_time > 10:
     st.session_state.last_refresh_time = time.time()
     st.experimental_rerun()
 
-# 載入 UI
+# 主畫面邏輯
 from ui_module import render_ui
 render_ui()
